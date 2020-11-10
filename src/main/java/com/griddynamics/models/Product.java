e86f9916cfbfbbbfc862993a30c6d657d079d6b4
Sergey Borisenko <sborisenko@griddynamics.com>
2020-11-10 13:37:20 +0200

    Refactor lombok annotation

--- src/main/java/com/griddynamics/models/Product.java
+++ src/main/java/com/griddynamics/models/Product.java
@@ -1,12 +1,8 @@
 package com.griddynamics.models;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.Data;
 
-@Getter
-@Setter
-@ToString
+@Data
 public class Product {
     private int id;
     private String name;
