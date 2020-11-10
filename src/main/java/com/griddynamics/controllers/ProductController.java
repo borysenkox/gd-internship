190be57b6b1001fa5b49eeb2b723204c1ac94685
Sergey Borisenko <sborisenko@griddynamics.com>
2020-11-10 12:23:00 +0200

    Implements ProductController methods

--- src/main/java/com/griddynamics/controllers/ProductController.java
+++ src/main/java/com/griddynamics/controllers/ProductController.java
@@ -1,33 +1,89 @@
 package com.griddynamics.controllers;
 
+import com.griddynamics.models.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import service.ProductService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/shop")
 public class ProductController {
 
-    @GetMapping("/product")
-    public String getAllProducts() {
-        return "In getAllProducts()";
+    private final ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
     }
 
-    @GetMapping("/product/{id}")
-    public String getProductById(@PathVariable("id") int id) {
-        return "In getProductById()";
+    @GetMapping(value = "/product", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<Product>> getAllProducts() {
+        List<Product> products = this.productService.findAll();
+
+        if (products == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/product/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Product> getProductById(@PathVariable Integer id) {
+        if (id == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        Product product = this.productService.getById(id);
+
+        if (product == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(product, HttpStatus.OK);
     }
 
     @PutMapping("/product")
-    public String addNewProduct() {
-        return "In addNewProduct()";
+    public ResponseEntity<Product> addNewProduct(@RequestBody Product product) {
+        if (product == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        this.productService.save(product);
+
+        return new ResponseEntity<>(product, HttpStatus.CREATED);
     }
 
-    @PostMapping("/product/{id}")
-    public String editProductById(@PathVariable("id") int id) {
-        return "In editProductById()";
+    @PutMapping(value = "/product/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Product> editProductById(@PathVariable Integer id, @RequestBody Product product) {
+
+        if (id == null || product == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        this.productService.update(id, product);
+
+        return new ResponseEntity<>(product, HttpStatus.OK);
     }
 
-    @DeleteMapping("/product/{id}")
-    public String deleteProductById(@PathVariable("id") int id) {
-        return "In deleteProductById()";
+    @DeleteMapping(value = "/product/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Product> deleteProduct(@PathVariable Integer id) {
+        if (id == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        Product product = this.productService.getById(id);
+
+        if (product == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        this.productService.deleteById(id);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
