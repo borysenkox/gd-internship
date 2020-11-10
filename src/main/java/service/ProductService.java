86f389eae53774c93b9b45cf7df3dabf6a9b0bc7
Sergey Borisenko <sborisenko@griddynamics.com>
2020-11-10 12:27:07 +0200

    Initial ProductService

---
+++ src/main/java/service/ProductService.java
@@ -0,0 +1,30 @@
+package service;
+
+import com.griddynamics.models.Product;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    public List<Product> findAll() {
+        return null;
+    }
+
+    public Product getById(Integer id) {
+        return null;
+    }
+
+    public void save(Product product) {
+
+    }
+
+    public void update(Integer id, Product product) {
+
+    }
+
+    public void deleteById(Integer id) {
+
+    }
+}
