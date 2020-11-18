c75e3275457a6a3583317fef553f9eb5b223ed67
volvozachto <fogmrfog@gmail.com>
2020-11-18 16:33:48 +0200

    Add @EqualsAndHashCode (callSuper = true)

--- src/main/java/com/griddynamics/entities/Category.java
+++ src/main/java/com/griddynamics/entities/Category.java
@@ -1,11 +1,13 @@
 package com.griddynamics.entities;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 @Entity(name = "Category")
 @Table(name = "category")
