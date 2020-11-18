cde6d4b6cc551af813304ba593d047638c1613fd
volvozachto <fogmrfog@gmail.com>
2020-11-18 16:34:49 +0200

    Override hashcode() and equals(Object).

--- src/main/java/com/griddynamics/entities/AbstractEntity.java
+++ src/main/java/com/griddynamics/entities/AbstractEntity.java
@@ -7,11 +7,31 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
+import java.util.Objects;
 
 @Data
 @MappedSuperclass
-public class AbstractEntity implements Serializable {
+public abstract class AbstractEntity implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     protected Integer id;
+
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (!getClass().equals(obj.getClass()) ) {
+            return false;
+        }
+        AbstractEntity ent = (AbstractEntity) obj;
+        return id.equals(ent.id);
+    }
 }
