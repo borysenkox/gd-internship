22b34dba665f9837c3db6cc2404691d3ef8b70aa
volvozachto <fogmrfog@gmail.com>
2020-11-18 11:47:48 +0200

    Refactor ProductDTO Add new field List<CategoryDTO> Make DTO implementing Serializable

--- src/main/java/com/griddynamics/dto/ProductDTO.java
+++ src/main/java/com/griddynamics/dto/ProductDTO.java
@@ -1,13 +1,56 @@
 package com.griddynamics.dto;
 
+import com.griddynamics.entities.Product;
 import lombok.Data;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
-public class ProductDTO {
-    private int id;
+public class ProductDTO implements Serializable {
+
+    private Integer id;
+
     private String name;
-    private double price;
+
+    private Double price;
+
     private String description;
+
     private String brand;
+
     private String image;
+
+    private List<CategoryDTO> categoryDTOList;
+
+    public ProductDTO() { }
+
+    public ProductDTO(int id, String name, double price, String description, String brand, String image,
+                      List<CategoryDTO> categoryDTOList) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.brand = brand;
+        this.image = image;
+
+        this.categoryDTOList = new ArrayList<>();
+
+        if (categoryDTOList != null) {
+            this.categoryDTOList = new ArrayList<>(categoryDTOList);
+        }
+    }
+
+    public ProductDTO(Product product) {
+        id = product.getId();
+        name = product.getName();
+        price = product.getPrice();
+        description = product.getDescription();
+        brand = product.getBrand();
+        image = product.getImage();
+        categoryDTOList = new ArrayList<>();
+    }
+
+
 }
