952d3725923993883991c4bd5690b02802dca4b6
volvozachto <fogmrfog@gmail.com>
2020-11-20 13:59:54 +0200

    Delete of overridden equals(Object) and hashcode().

--- src/main/java/com/griddynamics/dto/CategoryDTO.java
+++ src/main/java/com/griddynamics/dto/CategoryDTO.java
@@ -2,10 +2,11 @@ package com.griddynamics.dto;
 
 import com.griddynamics.entities.Category;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.util.Objects;
 
-
+@EqualsAndHashCode(callSuper = true)
 @Data
 public class CategoryDTO extends AbstractDTO {
 
@@ -25,23 +26,5 @@ public class CategoryDTO extends AbstractDTO {
         id = category.getId();
         name = category.getName();
         parent = category.getParent();
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (id != null) return super.equals(o);
-        if (this == o) return true;
-        if (!(o instanceof CategoryDTO)) return false;
-        if (!super.equals(o)) return false;
-        CategoryDTO that = (CategoryDTO) o;
-        return Objects.equals(name, that.name) &&
-                Objects.equals(parent, that.parent);
-    }
-
-    @Override
-    public int hashCode() {
-        if (id != null) return super.hashCode();
-
-        return Objects.hash(super.hashCode(), name, parent);
     }
 }
