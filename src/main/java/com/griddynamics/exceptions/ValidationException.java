a05c831431a7f2d23063927e6db37c5b8c3842ad
volvozachto <fogmrfog@gmail.com>
2020-11-20 16:15:20 +0200

    Add ValidationException that throws in Validator implementations.

---
+++ src/main/java/com/griddynamics/exceptions/ValidationException.java
@@ -0,0 +1,44 @@
+package com.griddynamics.exceptions;
+
+public class ValidationException extends RuntimeException {
+    public ValidationException() {
+        super();
+    }
+
+    public ValidationException(String message) {
+        super(message);
+    }
+
+    public ValidationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ValidationException(Throwable cause) {
+        super(cause);
+    }
+
+    @Override
+    public String getMessage() {
+        return super.getMessage();
+    }
+
+    @Override
+    public String getLocalizedMessage() {
+        return super.getLocalizedMessage();
+    }
+
+    @Override
+    public synchronized Throwable getCause() {
+        return super.getCause();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    @Override
+    public void printStackTrace() {
+        super.printStackTrace();
+    }
+}
